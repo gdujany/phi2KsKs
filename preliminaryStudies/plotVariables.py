fb39281e10290193aa68aaf1de2b4d64ad1b70a5
Sonja Verena Bartkowski <sbartkow@lxplus0088.cern.ch>
2015-07-07 15:15:25 +0200

    Additional variables in plotVariables.py and trackTypes to determine if Ks decays in beam pipe

--- preliminaryStudies/plotVariables.py
+++ preliminaryStudies/plotVariables.py
@@ -28,14 +28,22 @@ inFiles = {}
 inFiles['data2012'] = r.TFile(os.path.join(store_dir, 'data2012.root'))
 inFiles['phi2KsKs_incl'] = r.TFile(os.path.join(store_dir, 'phi2KsKs_incl.root'))
 inFiles['phi2KsKs_Ds'] = r.TFile(os.path.join(store_dir, 'phi2KsKs_Ds.root'))
+inFiles['minbias'] = r.TFile(os.path.join(store_dir, 'minbias.root'))
+
 
 trees = {}
 for key, inFile in inFiles.items():
     trees[key] = inFile.Get('TuplePhi2KsKs/DecayTree')
 
+cut = 'phi_PT<5100 && pi1_PT>200 && pi2_PT> 200 && pi3_PT>200 && pi4_PT>200 &&(sqrt(Ks1_ENDVERTEX_X*Ks1_ENDVERTEX_X + Ks1_ENDVERTEX_Y*Ks1_ENDVERTEX_Y) < 7 ||sqrt(Ks2_ENDVERTEX_X*Ks2_ENDVERTEX_X + Ks2_ENDVERTEX_Y*Ks2_ENDVERTEX_Y) < 7 )'
+
+
+
 ct = CompareTreeVars(normalise=True)
-ct.addTree('data2012', trees['data2012'], evtMax=50000, tree_cut = '')
-ct.addTree('phi2KsKs_incl', trees['phi2KsKs_incl'], tree_cut = '')
+ct.addTree('data2012', trees['data2012'], evtMax=50000, tree_cut = cut)
+ct.addTree('phi2KsKs_incl', trees['phi2KsKs_incl'], tree_cut = cut)
+ct.addTree('minbias', trees['minbias'], tree_cut = cut)
+
 
 # Choose variables to plot
 
@@ -44,7 +52,30 @@ ct.addTree('phi2KsKs_incl', trees['phi2KsKs_incl'], tree_cut = '')
 ct.addVariable('phi_M', region = [990, 1110])
 ct.addVariable('phi_PT', region = [0, 15000])
 ct.addVariable('phi_P', region = [0, 300000])
-
+ct.addVariable('phi_PE', region = [0, 300e3])
+ct.addVariable('phi_ENDVERTEX_CHI2', region = [0, 80])
+ct.addVariable('Ks1_CosTheta', region = [-1, 1])
+ct.addVariable('Ks1_ENDVERTEX_CHI2', region = [0, 4])
+ct.addVariable('Ks1_M', region = [497.55, 497.65])
+ct.addVariable('Ks1_PT', region = [0, 10000])
+ct.addVariable('Ks1_P', region = [0, 200e3])
+ct.addVariable('Ks2_CosTheta', region = [-1, 1])
+ct.addVariable('Ks2_ENDVERTEX_CHI2', region = [0, 4])
+ct.addVariable('Ks2_M', region = [497.55, 497.65])
+ct.addVariable('Ks2_PT', region = [0, 10000])
+ct.addVariable('Ks2_P', region = [0, 200e3])
+ct.addVariable('pi1_CosTheta', region = [-1, 1])
+ct.addVariable('pi1_PT', region = [0, 6000])
+ct.addVariable('pi1_P', region = [0, 100e3])
+ct.addVariable('pi2_CosTheta', region = [-1, 1])
+ct.addVariable('pi2_PT', region = [0, 6000])
+ct.addVariable('pi2_P', region = [0, 100e3])
+ct.addVariable('pi3_CosTheta', region = [-1, 1])
+ct.addVariable('pi3_PT', region = [0, 6000])
+ct.addVariable('pi3_P', region = [0, 100e3])
+ct.addVariable('pi4_CosTheta', region = [-1, 1])
+ct.addVariable('pi4_PT', region = [0, 6000])
+ct.addVariable('pi4_P', region = [0, 100e3])
 
 # Output names
 
